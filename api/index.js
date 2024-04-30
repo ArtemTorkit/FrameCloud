ec53c1aecfd60273f2bc65ce031032e9ced4b48e
ArtemTorkit <166482360+ArtemTorkit@users.noreply.github.com>
2024-05-01 00:08:15 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+//const cors = require('cors')
 const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser")
     
@@ -9,14 +9,30 @@ dotenv.config();
 const app = express()
 
 // Simplified CORS configuration
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true,
-};
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "http://localhost:5173"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
+
 
 // Use the cors middleware with the specified options
-app.use(cors(corsOptions));
+
 app.use(express.json({ limit: '30mb'}))
 app.use(cookieParser())
 
@@ -35,22 +51,6 @@ app.use('/user', userRoutes);
 //add postFetching route
 const postFetchingRoutes = require('./routes/postFetching');
 app.use('/post-fetching', postFetchingRoutes);
-
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "http://localhost:5173"
-        //"http://localhost:5173"
-    )
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    )
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST"
-    )
-})
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...')
